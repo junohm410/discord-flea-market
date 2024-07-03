4fb334188c0fcbfcfbdfb5356c243a069e37d379
junohm410 <junohm410@gmail.com>
2024-07-03 20:35:54 +0900

    Omniauthコールバック処理を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  devise :omniauthable, omniauth_providers: [:discord]
+
+  def self.find_or_create_from_auth_hash!(auth_hash)
+    provider = auth_hash['provider']
+    uid = auth_hash['uid']
+    name = auth_hash['info']['name']
+    avatar_url = auth_hash['info']['image']
+
+    User.find_or_create_by!(provider:, uid:) do |user|
+      user.name = name
+      user.avatar_url = avatar_url
+    end
+  end
 end
