97805b48dfcd16ef3462d564ff3f354122a8eb0c
junohm410 <junohm410@gmail.com>
2024-07-03 21:41:36 +0900

    仮のTOPページ・ログイン後の商品一覧ページと、ログイン・ログアウトボタンを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # TODO: 仮のログイン後のページに遷移するための実装。他のitemsリソースを追加した際に修正する。
+  get '/items', to: 'items#index', as: :items
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
+
+  root to: "welcome#index"
 end
