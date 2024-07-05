bf33b4771df4f40b1128898112104ffdb6b2a737
junohm410 <junohm410@gmail.com>
2024-07-05 13:21:09 +0900

    購入希望 / 取り消しボタンを実装

--- app/controllers/purchase_requests_controller.rb
+++ app/controllers/purchase_requests_controller.rb
@@ -1,7 +1,13 @@
 class PurchaseRequestsController < ApplicationController
   def create
+    item = Item.find(params[:item_id])
+    current_user.purchase_requests.create!(item:)
+    redirect_to item, notice: '購入希望を出しました'
   end
 
   def destroy
+    purchase_request = current_user.purchase_requests.find(params[:id])
+    purchase_request.destroy!
+    redirect_to purchase_request.item, notice: '購入希望を取り消しました', status: :see_other
   end
 end
