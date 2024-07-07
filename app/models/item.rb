4ec5bd5ac85e540e41b745d3848feec764fd7afa
junohm410 <junohm410@gmail.com>
2024-07-07 20:53:28 +0900

    購入者が確定してフリマが終わった商品にもアクセスできるようにスコープを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
   validates :deadline, presence: true
   validate :deadline_later_than_today, unless: -> { validation_context == :select_buyer }
 
-  scope :accessible_for, ->(user) { where(user:).or(listed) }
+  scope :accessible_for, ->(user) { where(user:).or(not_unpublished) }
   scope :closed_yesterday, -> { listed.where('deadline < ?', Time.current.beginning_of_day) }
   scope :by_status, lambda { |status|
     case status
