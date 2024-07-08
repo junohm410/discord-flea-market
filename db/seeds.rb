bc36a9fd93f0c7379c1147cd91e68a4fb05aeca1
junohm410 <junohm410@gmail.com>
2024-07-08 22:02:19 +0900

    seedsファイルの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,63 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+UID_DIGITS = 19
+
+4.times do |n|
+  User.create!(
+    uid: format("%0#{UID_DIGITS}d", SecureRandom.random_number(10**UID_DIGITS)),
+    name: "user#{n + 1}",
+    provider: 'discord',
+    avatar_url: nil
+  )
+end
+
+user1 = User.first
+user2 = User.second
+user3 = User.third
+user4 = User.fourth
+
+2.times do |n|
+  user1.items.create!(
+    name: "締切が一ヶ月後の商品#{n + 1}",
+    description: "締切が一ヶ月後の商品#{n + 1}の説明",
+    price: 1000,
+    shipping_cost_covered: true,
+    payment_method: '楽天ペイ',
+    deadline: Time.current.next_month,
+    status: :listed
+  )
+end
+
+2.times do |n|
+  item = user1.items.new(
+    name: "締切が昨日の商品#{n + 1}",
+    description: "締切が昨日の商品#{n + 1}の説明",
+    price: 1000,
+    shipping_cost_covered: true,
+    payment_method: 'PayPay',
+    deadline: Time.current.yesterday,
+    status: :listed
+  )
+  item.save!(validate: false)
+end
+
+user1.items.new(
+  name: '購入者確定済みの商品',
+  description: '購入者確定済みの商品',
+  price: 1000,
+  shipping_cost_covered: true,
+  payment_method: '楽天ペイ',
+  deadline: Time.current.prev_month,
+  status: :buyer_selected,
+  buyer: user2
+).save!(validate: false)
+
+listed_item = Item.listed.first
+yesterdays_deadline_item = Item.closed_yesterday.first
+buyer_selected_item = Item.buyer_selected.first
+
+PurchaseRequest.create!(item: listed_item, user: user2)
+PurchaseRequest.create!(item: yesterdays_deadline_item, user: user2)
+PurchaseRequest.create!(item: yesterdays_deadline_item, user: user3)
+PurchaseRequest.create!(item: buyer_selected_item, user: user2)
+PurchaseRequest.create!(item: buyer_selected_item, user: user4)
