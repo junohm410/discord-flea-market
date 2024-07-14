b10cbcb8e9af97a28d8adb7e3307003d1358b9ee
junohm410 <junohm410@gmail.com>
2024-07-14 20:28:47 +0900

    Userモデルのファクトリの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@
 
 FactoryBot.define do
   factory :user do
+    name { 'alice' }
+    uid { '1234567' }
+    provider { 'discord' }
+    avatar_url { 'https://cdn.discordapp.com/embed/avatars/1.png' }
   end
 end
