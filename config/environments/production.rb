8f92d97d61f94859a1403bd116721dffea5633c8
junohm410 <junohm410@gmail.com>
2024-07-14 18:04:13 +0900

    any_loginを全環境で読み込みつつ、productionで無効にする

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  AnyLogin.setup do |config|
+    config.enabled = false
+  end
 end
