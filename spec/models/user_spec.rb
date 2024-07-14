dcf353e8ce828ae14eb3f45e51b5a8948fd74927
junohm410 <junohm410@gmail.com>
2024-07-14 21:40:21 +0900

    User.find_or_create_from_auth_hash!のテストの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#find_or_create_from_auth_hash!' do
+    let(:uid) { '2345678' }
+    let(:auth_hash) do
+      {
+        provider: 'discord',
+        uid:,
+        info: {
+          name: 'bob',
+          image: 'https://cdn.discordapp.com/embed/avatars/1.png'
+        }
+      }
+    end
+
+    context 'when the user is new' do
+      it 'creates and returns a new user' do
+        user = User.find_or_create_from_auth_hash!(auth_hash)
+        expect(user.provider).to eq 'discord'
+        expect(user.uid).to eq '2345678'
+        expect(user.name).to eq 'bob'
+        expect(user.avatar_url).to eq 'https://cdn.discordapp.com/embed/avatars/1.png'
+        expect(User.find_or_create_from_auth_hash!(auth_hash)).to eq User.find_by(uid:)
+      end
+
+      it 'increases the user count by 1' do
+        expect { User.find_or_create_from_auth_hash!(auth_hash) }.to change(User, :count).from(0).to(1)
+      end
+    end
+
+    context 'when the user already exists' do
+      let!(:existing_user) { FactoryBot.create(:user, uid:) }
+
+      it 'returns the existing user' do
+        user = User.find_or_create_from_auth_hash!(auth_hash)
+        expect(user).to eq existing_user
+      end
+
+      it "doesn't change the user count" do
+        expect { User.find_or_create_from_auth_hash!(auth_hash) }.not_to change(User, :count)
+      end
+    end
+  end
 end
