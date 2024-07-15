9765cb61e8611f14de88f00f447f6331ade7b388
junohm410 <junohm410@gmail.com>
2024-07-15 22:58:29 +0900

    Itemモデルのスコープ・カスタムバリデータのテストを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,5 +3,45 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:alice) { FactoryBot.create(:user) }
+
+  describe '.accessible_for' do
+    it 'returns their own items or not unpublished items by other users' do
+      bob = FactoryBot.create(:user, name: 'bob')
+      carol = FactoryBot.create(:user, name: 'carol')
+
+      alices_item = FactoryBot.create(:item, user: alice)
+      bobs_listing_item = FactoryBot.create(:item, user: bob)
+      bobs_buyer_selected_item = FactoryBot.create(:buyer_selected_item, user: bob, buyer: carol)
+      FactoryBot.create(:purchase_request, item: bobs_buyer_selected_item, user: carol)
+      FactoryBot.create(:unpublished_item, user: bob)
+
+      expect(Item.accessible_for(alice)).to contain_exactly(alices_item, bobs_listing_item, bobs_buyer_selected_item)
+    end
+  end
+
+  describe '.closed_yesterday' do
+    it 'returns items whose deadline is yesterday' do
+      closed_yesterday_item = FactoryBot.build(:item, user: alice, deadline: Time.current.yesterday)
+      closed_yesterday_item.save!(validate: false)
+      FactoryBot.create(:item, user: alice, deadline: Time.current.beginning_of_day)
+      FactoryBot.create(:item, user: alice, deadline: Time.current.tomorrow)
+
+      expect(Item.closed_yesterday).to contain_exactly closed_yesterday_item
+    end
+  end
+
+  describe 'validate deadline_later_than_today' do
+    it "validates that the deadline can't be earlier than today" do
+      item = FactoryBot.build(:item, user: alice, deadline: Time.current.yesterday)
+      item.valid?
+      expect(item.errors[:deadline]).to include "can't be earlier than today"
+    end
+
+    it 'validates that the deadline can be later than today' do
+      item = FactoryBot.build(:item, user: alice, deadline: Time.current.tomorrow)
+      item.valid?
+      expect(item.errors[:deadline]).to be_empty
+    end
+  end
 end
