ddaae73f4c1acb59920c467dc8d018883ca64e54
junohm410 <junohm410@gmail.com>
2024-07-27 14:18:54 +0900

    Item#changed_to_listed_from_unpublished?のテストのコンテキストの追加
    Item#changed_to_unpublished_from_listed?でステータスの変更がない場合のコンテキストを用意したので、それを合わせた

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,6 +63,18 @@ RSpec.describe Item, type: :model do
         expect(item.changed_to_listed_from_unpublished?).to be false
       end
     end
+
+    context 'when an item status has not changed' do
+      it 'returns false' do
+        listed_item = FactoryBot.create(:item, user: alice)
+        listed_item.update(name: '更新後の商品名')
+        expect(listed_item.changed_to_unpublished_from_listed?).to be false
+
+        unpublished_item = FactoryBot.create(:unpublished_item, user: alice)
+        unpublished_item.update(name: '更新後の商品名')
+        expect(unpublished_item.changed_to_unpublished_from_listed?).to be false
+      end
+    end
   end
 
   describe 'changed_to_unpublished_from_listed?' do
