03c63f117e9b9e481daa988016c9a97b7272b9f8
junohm410 <junohm410@gmail.com>
2024-08-11 22:16:41 +0900

    クラスメソッドの定義の仕方を見直し、共通処理をプライベートメソッドに移す

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,39 +13,51 @@ class User < ApplicationRecord
   validates :uid, presence: true, uniqueness: { scope: :provider }
   validates :provider, presence: true, inclusion: { in: %w[discord] }
 
-  def self.find_or_create_from_auth_hash!(auth_hash)
-    provider = auth_hash[:provider]
-    uid = auth_hash[:uid]
-    name = auth_hash[:info][:name]
-    avatar = auth_hash[:extra][:raw_info][:avatar]
+  class << self
+    def find_or_create_from_auth_hash!(auth_hash)
+      provider = auth_hash[:provider]
+      uid = auth_hash[:uid]
+      name = auth_hash[:info][:name]
+      avatar = auth_hash[:extra][:raw_info][:avatar]
 
-    # サーバーメンバーではない場合リクエストに失敗してログインできない
-    Discordrb::API::Server.resolve_member("Bot #{ENV['DISCORD_BOT_TOKEN']}", ENV['DISCORD_SERVER_ID'], uid)
+      # サーバーメンバーではない場合リクエストに失敗してログインできない
+      Discordrb::API::Server.resolve_member("Bot #{ENV['DISCORD_BOT_TOKEN']}", ENV['DISCORD_SERVER_ID'], uid)
 
-    User.find_or_create_by!(provider:, uid:) do |user|
-      user.name = name
-      user.avatar_url = avatar ? Discordrb::API::User.avatar_url(uid, avatar) : Discordrb::API::User.default_avatar
+      User.find_or_create_by!(provider:, uid:) do |user|
+        user.name = name
+        user.avatar_url = make_avatar_url(uid, avatar)
+      end
     end
-  end
 
-  def self.remove_by_member_leaving_event(event)
-    uid = event.user.id
-    user = find_by(uid:)
-    user&.destroy
-  end
+    def remove_by_member_leaving_event(event)
+      uid = event.user.id
+      user = find_by(uid:)
+      user&.destroy
+    end
 
-  def self.update_by_member_updating_event(event)
-    uid = event.user.id
-    updated_member = JSON.parse(Discordrb::API::User.resolve("Bot #{ENV['DISCORD_BOT_TOKEN']}", uid))
+    def update_by_member_updating_event(event)
+      uid = event.user.id
+      updated_member = JSON.parse(Discordrb::API::User.resolve("Bot #{ENV['DISCORD_BOT_TOKEN']}", uid))
 
-    name = updated_member['username']
-    avatar = updated_member['avatar']
-    avatar_url = avatar ? Discordrb::API::User.avatar_url(uid, avatar) : Discordrb::API::User.default_avatar
+      name = updated_member['username']
+      avatar = updated_member['avatar']
+      avatar_url = make_avatar_url(uid, avatar)
 
-    user = User.find_by(uid:)
-    return unless user
+      user = User.find_by(uid:)
+      return unless user
 
-    user.update!(name:) if user.name != name
-    user.update!(avatar_url:) if user.avatar_url != avatar_url
+      user.update!(name:) if user.name != name
+      user.update!(avatar_url:) if user.avatar_url != avatar_url
+    end
+
+    private
+
+    def make_avatar_url(uid, avatar)
+      if avatar
+        Discordrb::API::User.avatar_url(uid, avatar)
+      else
+        Discordrb::API::User.default_avatar
+      end
+    end
   end
 end
